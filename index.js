fbd7ed948288c816c0b63c19560588019b45c0b8
Fernanda Romero <fernandaromero85@gmail.com>
2019-06-07 20:16:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if ((string) === lowercase
+  if ((string) === lowercase {
+    return "I can\'t hear you!"
+  }
 }
 
 it('returns "I can\'t hear you!" if `string` is lowercase', function() {
